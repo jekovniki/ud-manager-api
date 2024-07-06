3c4b5132966749ba6080eacce612e40d6c3b88c2
jekovniki <jekovniki95@gmail.com>
2024-07-06 17:01:09 +0300

    ref(folder): reorginise folders ref(users): remove create user endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { DatabaseModule } from "./database/database.module";
 import { CompaniesModule } from "./companies/companies.module";
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { CommonModule } from "./common/common.module";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
 		}),
-		DatabaseModule,
 		CompaniesModule,
 		AuthModule,
 		UsersModule,
+		CommonModule,
 	],
 })
 export class AppModule {}
