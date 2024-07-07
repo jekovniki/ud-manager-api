d496dfad96130d6669467b711fc374504227d63e
jekovniki <jekovniki95@gmail.com>
2024-07-07 23:20:22 +0300

    feature(email in progress) implement the nestjs mailer module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "./database/database.module";
+import { EmailModule } from './email/email.module';
 
 @Module({
-	imports: [DatabaseModule],
+	imports: [DatabaseModule, EmailModule],
 })
 export class CommonModule {}
