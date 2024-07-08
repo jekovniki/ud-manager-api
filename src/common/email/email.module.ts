2081ac5341370d30f39fe6777d6a754c01fad91e
jekovniki <jekovniki95@gmail.com>
2024-07-08 21:19:54 +0300

    feat(email): implement email module

--- src/common/email/email.module.ts
+++ src/common/email/email.module.ts
@@ -1,7 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { MailerModule } from "@nestjs-modules/mailer";
 import { EmailService } from './email.service';
 
 @Module({
-  providers: [EmailService]
+	imports: [
+		MailerModule.forRoot({
+			transport: {
+				host: process.env.EMAIL_HOST,
+				auth: {
+					user: process.env.EMAIL_USERNAME,
+					pass: process.env.EMAIL_PASSWORD,
+				},
+			},
+		}),
+	],
+	providers: [EmailService],
 })
 export class EmailModule {}
--- src/common/email/email.service.ts
+++ src/common/email/email.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { MailerService } from "@nestjs-modules/mailer";
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+	private readonly companyEmail = "";
+	constructor(private readonly mailService: MailerService) {}
+
+	public sendMail(to: string, subject: string, text: string, from: string) {
+		this.mailService.sendMail({
+			from,
+			to,
+			subject,
+			text,
+		});
+	}
+
+	public sendRegistrationMail(to: string) {
+		this.sendMail(
+			to,
+			"AMC Manager | Регистрация",
+			"You're receiving this email, because your company have registered",
+			this.companyEmail,
+		);
+	}
+}
