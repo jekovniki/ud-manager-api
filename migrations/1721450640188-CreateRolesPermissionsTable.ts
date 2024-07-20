00c45e889ab41e2d205ed980a1979d71770414bc
jekovniki <jekovniki95@gmail.com>
2024-07-20 08:05:37 +0300

    feat(role-permission): add role-permission table migration

---
+++ migrations/1721450640188-CreateRolesPermissionsTable.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { Logger } from "@nestjs/common";
+
+export class CreateRolesPermissionsTable1721450640188
+	implements MigrationInterface
+{
+	private readonly logger = new Logger(
+		CreateRolesPermissionsTable1721450640188.name,
+	);
+
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		this.logger.log("UP - START");
+		await queryRunner.createTable(
+			new Table({
+				name: "role-permission",
+				columns: [
+					{
+						name: "id",
+						type: "int",
+						isPrimary: true,
+						isGenerated: true,
+						generationStrategy: "increment",
+					},
+					{
+						name: "role_id",
+						type: "int",
+					},
+					{
+						name: "permission_id",
+						type: "int",
+					},
+				],
+			}),
+			true,
+		);
+		this.logger.log("UP - COMPLETED");
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		this.logger.log("DOWN - START");
+		await queryRunner.dropTable("role-permission");
+		this.logger.log("DOWN - COMPLETED");
+	}
+}
