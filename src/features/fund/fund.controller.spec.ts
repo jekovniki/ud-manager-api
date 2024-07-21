90c2c1e01d41adced05290fbe9b55bec607c6d7f
jekovniki <jekovniki95@gmail.com>
2024-07-21 08:37:41 +0300

    ref(format): fund controller

--- src/features/fund/fund.controller.spec.ts
+++ src/features/fund/fund.controller.spec.ts
@@ -1,20 +1,20 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { FundController } from './fund.controller';
-import { FundService } from './fund.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { FundController } from "./fund.controller";
+import { FundService } from "./fund.service";
 
-describe('FundController', () => {
-  let controller: FundController;
+describe("FundController", () => {
+	let controller: FundController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [FundController],
-      providers: [FundService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [FundController],
+			providers: [FundService],
+		}).compile();
 
-    controller = module.get<FundController>(FundController);
-  });
+		controller = module.get<FundController>(FundController);
+	});
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+	it("should be defined", () => {
+		expect(controller).toBeDefined();
+	});
 });
