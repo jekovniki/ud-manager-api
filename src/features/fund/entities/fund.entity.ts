5154c9a94468f82e3fbf6e34a21657d6ea24e893
jekovniki <jekovniki95@gmail.com>
2024-07-21 07:30:46 +0300

    feat(fund): add entity

--- src/features/fund/entities/fund.entity.ts
+++ src/features/fund/entities/fund.entity.ts
@@ -1,1 +1,43 @@
-export class Fund {}
+import {
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	ManyToOne,
+	JoinColumn,
+	Index,
+	CreateDateColumn,
+	UpdateDateColumn,
+} from "typeorm";
+import { Company } from "../../company/entities/company.entity";
+
+@Entity("fund")
+export class Fund {
+	@PrimaryGeneratedColumn("uuid")
+	id: string;
+
+	@Column({ unique: true })
+	name: string;
+
+	@Column()
+	description: string;
+
+	@Column({ unique: true })
+	bullstat: string;
+
+	@Column("uuid")
+	@Index("IDX_FUND_COMPANY_ID")
+	company_id: string;
+
+	@ManyToOne(() => Company)
+	@JoinColumn({ name: "company_id" })
+	company: Company;
+
+	@Column({ default: true })
+	active: boolean;
+
+	@CreateDateColumn({ type: "timestamptz", default: () => "CURRENT_TIMESTAMP" })
+	created_at: Date;
+
+	@UpdateDateColumn({ type: "timestamptz", default: () => "CURRENT_TIMESTAMP" })
+	updated_at: Date;
+}
